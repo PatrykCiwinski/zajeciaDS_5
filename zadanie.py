6e19e01cba66dd233eeded1cd788a5c298c0e7b8
Patryk <patryk.ciwinski@gmail.com>
2023-02-21 19:44:41 +0100

    minor changes

--- zadanie.py
+++ zadanie.py
@@ -1,3 +1,4 @@
 lista_skladana=[i for i in range(1, 101) if i % 5 == 0]
-
+s = 0
+print([s:=s+i for i in range(1, 101) if i%5 == 0][-1]) #printuje wszystkie sumy , dlatego [-1], żby ostatni wynik
 print(sum(lista_skladana))
