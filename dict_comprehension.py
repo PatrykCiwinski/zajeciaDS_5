d977824a6c86f4eb14ca03e132f87222e2e8d2f7
Patryk <patryk.ciwinski@gmail.com>
2023-02-21 20:19:18 +0100

    moooore training

---
+++ dict_comprehension.py
@@ -0,0 +1,30 @@
+var=34
+dt={(1,2,3):['a','b','c'],
+    3.14: 'pi',
+    'e':2,
+    1:'zero',
+    var:'text'}
+
+# for key in dt:
+#     print(dt[key])
+
+print(dt['e'])
+
+#metoda get lepsza bo zwraca none nie keyerror, albo to co damy jak 2 argument
+
+print(dt.get('y'))
+print(dt.get('y', 'This key doesn\'t exist'))
+
+dc=dict(zip(['x','y','z'],(1,2,3)))
+
+print()
+print(dc)
+dc1=dict(zip([i for i in range(1,11)],('abcdefghij')))
+
+dc2={k+10:v for (k,v) in dc1.items()}
+
+print()
+print(dc1)
+
+print()
+print(dc2)
